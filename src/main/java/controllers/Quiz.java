6936d518702272b10e134142276d6fa533c46c54
93175 <93175@farnborough.ac.uk>
2020-11-27 16:02:59 +0000

    Added questions api and started html quiz for questions

--- src/main/java/controllers/Quiz.java
+++ src/main/java/controllers/Quiz.java
@@ -4,10 +4,7 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import server.Main;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -37,5 +34,28 @@ public class Quiz {
             return "{\"Error\": \"Unable to list items.  Error code xx.\"}";
         }
     }
-}
 
+
+
+    @GET
+    @Path("listQuestions/{quizId}")
+    public String questionList(@PathParam("quizId") Integer quizId) {
+        System.out.println("Invoked Questions.QuestionList()");
+        JSONArray response = new JSONArray();
+        try {
+            PreparedStatement ps = Main.db.prepareStatement("SELECT QuestionId, QuestionText FROM Questions WHERE QuizId = ?");
+            ps.setInt(1,quizId);
+            ResultSet results = ps.executeQuery();
+            while (results.next()==true) {
+                JSONObject row = new JSONObject();
+                row.put("QuestionId", results.getInt(1));
+                row.put("QuestionText", results.getString(2));
+                response.add(row);
+            }
+            return response.toString();
+        } catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"Error\": \"Unable to list items.  Error code xx.\"}";
+        }
+    }
+}
